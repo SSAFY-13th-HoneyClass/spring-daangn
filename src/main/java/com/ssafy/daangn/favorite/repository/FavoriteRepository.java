2a17f798d36505cc508716b537e2530cbca7c2b4
nodb <scnoh02@naver.com>
2025-05-16 03:36:14 +0900

    fix: Favorite 오타 수정

--- src/main/java/com/ssafy/daangn/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/ssafy/daangn/favorite/repository/FavoriteRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.daangn.board.entity.Board;
 import com.ssafy.daangn.favorite.entity.Favorite;
+import com.ssafy.daangn.favorite.entity.FavoriteId;
 import com.ssafy.daangn.member.entity.Member;
 
-public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+public interface FavoriteRepository extends JpaRepository<Favorite, FavoriteId> {
     Optional<Favorite> findByMemberAndBoard(Member member, Board board);
     List<Favorite> findByBoard(Board board);
     void deleteByMemberAndBoard(Member member, Board board);
