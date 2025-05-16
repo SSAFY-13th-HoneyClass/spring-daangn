e0e2e17ecdc26a9ca9cce789a36410fff7667890
yoonsu <rladbstn0425@gmail.com>
2025-05-16 14:22:25 +0900

    feat: ProductService 개발

---
+++ daangn_demo/src/main/java/com/ssafy/daangn_demo/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.ssafy.daangn_demo.service;
+
+import com.ssafy.daangn_demo.entity.ProductEntity;
+import com.ssafy.daangn_demo.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<ProductEntity> getByWriter(Long writerId) {
+        return productRepository.findAllByWriterId(writerId);
+    }
+
+}
